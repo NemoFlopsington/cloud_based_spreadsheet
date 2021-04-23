aa2d571905f11b19888ea66913c969175595a5ca
Nehemiah Deason <nijanemi@yahoo.com>
2021-04-23 10:05:00 -0500

    finished project -design documents

--- main_folder/src/main/java/ClientDriver.java
+++ main_folder/src/main/java/ClientDriver.java
@@ -1,21 +1,20 @@
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.Pane;
+import javafx.stage.Popup;
+import javafx.stage.PopupWindow;
 import javafx.stage.Stage;
 
-import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.io.IOException;
-import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
@@ -26,8 +25,12 @@ public class ClientDriver extends Application {
     final int width = 100;
     final int height = 100;
     String currentSelected;
+    TextField lastSelected;
     Cell[][] cells;
     TextField[][] cellsTf;
+    Scene scene;
+    TextField header;
+    Stage primaryStage;
 
 
     @Override
@@ -35,14 +38,22 @@ public class ClientDriver extends Application {
         //menu bar
         Menu fileMenu = new Menu("file");
         MenuItem save = new MenuItem("save");
+        MenuItem saveAs = new MenuItem("save as");
+        header = new TextField("Untitled Spreadsheet");
+        header.setStyle("-fx-font-size: 16");
+        saveAs.setOnAction(saveAsAction());
+        //TODO: impliment save function for save
         save.setOnAction(saveAction());
         MenuItem open = new MenuItem("open");
+        open.setOnAction(openAction());
         cells = new Cell[100][100];
         cellsTf = new TextField[100][100];
-        fileMenu.getItems().addAll(save, open);
+        fileMenu.getItems().addAll(save, saveAs, open);
         MenuBar menuBar = new MenuBar();
         menuBar.getMenus().addAll(fileMenu);
         menuBar.setStyle("-fx-background-color: green");
+        GridPane main = new GridPane();
+        scene = new Scene(main, 1000 , 1000);
 
         //cells display
         GridPane cellsDisplay = new GridPane();
@@ -65,22 +76,25 @@ public class ClientDriver extends Application {
                 cells[i][j] = new Cell(String.valueOf(i) + numToString(j),"", "");
                 cellsTf[i][j] = new TextField("");
                 cellsTf[i][j].setStyle("-fx-border-color: black");
-                cellsTf[i][j].setOnMouseClicked(selected(cells[i][j]));
-                cellsTf[i][j].setOnAction(cellChanged(cellsTf[i][j], cells[i][j]));
+                //cellsTf[i][j].addEventHandler(tab());
+                cellsTf[i][j].setOnMouseClicked(selected());
+                cellsTf[i][j].setOnKeyPressed(tabbed());
+                int finalI = i;
+                int finalJ = j;
+                cellsTf[i][j].textProperty().addListener(new ChangeListener<String>() {
+                    @Override
+                    public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                        cells[finalI][finalJ].content = newValue;
+                    }
+                });
                 cellsDisplay.add(cellsTf[i][j],j+1,i+1);
             }
         }
+        main.add(header,0,0);
+        main.add(menuBar,0,1);
+        main.add(new ScrollPane(cellsDisplay),0,2);
 
-
-
-
-
-        GridPane main = new GridPane();
-        main.add(menuBar,0,0);
-        main.add(new ScrollPane(cellsDisplay),0,1);
-
-        Scene scene = new Scene(main, 1000 , 1000);
-        Stage primaryStage = new Stage();
+        primaryStage = new Stage();
         primaryStage.setTitle("Cloud Based spreadsheet");
         primaryStage.setScene(scene);
         primaryStage.show();	//display stage
@@ -88,56 +102,158 @@ public class ClientDriver extends Application {
 
     }
 
-    private EventHandler<ActionEvent> cellChanged(TextField textField, Cell cell) {
-        return new EventHandler<ActionEvent>() {
+    private EventHandler<? super KeyEvent> tabbed() {
+        return new EventHandler<>() {
+
             @Override
-            public void handle(ActionEvent event) {
-                cell.content = textField.getText();
+            public void handle(KeyEvent event) {
+                if(event.getCode().equals(KeyCode.TAB)) {
+                    if(lastSelected != null)    {
+                        lastSelected.setStyle("-fx-border-color: #000000; -fx-border-width: 1");
+                    }
+                    scene.getFocusOwner().setStyle("-fx-border-color: green; -fx-border-width: 2");
+                    lastSelected = (TextField) scene.getFocusOwner();
+                    System.out.println("tabPressed");
+                }
             }
         };
     }
 
-    private EventHandler<? super MouseEvent> selected(Cell cell) {
+
+    private EventHandler<? super MouseEvent> selected() {
         //when cell is selected, change the previous cells border to black and change the current cells border to green
         return new EventHandler<>()    {
             @Override
             public void handle(MouseEvent event) {
-                if(currentSelected != null) {
-                    int[] oldCellRowColumn = row_columnToIndex(currentSelected);
-                    cellsTf[oldCellRowColumn[0]][oldCellRowColumn[1]].setStyle("-fx-border-color: #000000; -fx-border-width: 1");
+                if(lastSelected != null)    {
+                    lastSelected.setStyle("-fx-border-color: #000000; -fx-border-width: 1");
                 }
-                int[] newCellRowColumn = row_columnToIndex(cell.row_column);
-                cellsTf[newCellRowColumn[0]][newCellRowColumn[1]].setStyle("-fx-border-color: green; -fx-border-width: 2");
-                currentSelected = numToString(newCellRowColumn[0]) + newCellRowColumn[1];
+                scene.getFocusOwner().setStyle("-fx-border-color: green; -fx-border-width: 2");
+                lastSelected = (TextField) scene.getFocusOwner();
             }
         };
 
     }
 
-
-
-    private EventHandler<ActionEvent> saveAction() {
-        //TODO: impliment save button
+    private EventHandler<ActionEvent> openAction() {
         return new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 GridPane main = new GridPane();
+                Scene openScene = new Scene(main, 400,300);
+                Stage openStage = new Stage();
+                TextField pathTf = new TextField("Path to File");
+                Button submitBtn = new Button("Open File");
+                Label statusLbl = new Label();
+                submitBtn.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        clearPage();
+                        header.setText(pathTf.getText());
+                        Socket socket = null;
+                        ObjectOutputStream toServer = null;
+                        ObjectInputStream fromServer = null;
+                        try {
+                            socket = new Socket("localhost",8666);
+                            toServer = new ObjectOutputStream(socket.getOutputStream());
+                            fromServer = new ObjectInputStream(socket.getInputStream());
+                            toServer.writeObject(new Integer(0));
+                            toServer.writeObject(pathTf.getText());
+                            do {
+                                Cell currentCell = (Cell)fromServer.readObject();
+                                if(currentCell.content.equals("-1"))    //no more content found
+                                    break;
+                                if(currentCell.content.equals("-2")) {  //path not found
+                                    statusLbl.setText("file not found");
+                                    break;
+                                }
+                                int[] cellIndexes = row_columnToIndex(currentCell.row_column);
+                                cells[cellIndexes[0]][cellIndexes[1]] = currentCell;
+                                cellsTf[cellIndexes[0]][cellIndexes[1]].setText(currentCell.content);
+                            } while(true);
+                        } catch (IOException | ClassNotFoundException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+
+                main.add(submitBtn, 0, 1);
+                main.add(pathTf,0,0);
+                openStage.setScene(openScene);
+                openStage.show();
+                openStage.setTitle("Open");
+            }
+        };
+    }
+
+    private EventHandler<ActionEvent> saveAction() throws IOException, ClassNotFoundException {
+        return new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    Label statusLbl = new Label();
+                    statusLbl.setStyle("-fx-font-size: 16");
+                    Socket socket = new Socket("localhost", 8666);
+                    ObjectOutputStream toServer = new ObjectOutputStream(socket.getOutputStream());
+                    ObjectInputStream fromServer = new ObjectInputStream(socket.getInputStream());
+                    //write 1 to let server know we are saving a file
+                    toServer.writeObject(new Integer(1));
+                    toServer.writeUTF(header.getText());
+                    for (Cell[] cellRows : cells) {
+                        for (Cell cell : cellRows) {
+                            if (cell.content != "") toServer.writeObject(cell);
+                        }
+
+                    }
+                    //send terminator cell
+                    toServer.writeObject(new Cell("", "-1", ""));
+                    Integer status = (Integer) fromServer.readObject();
+                    if (status == -1) {
+                        statusLbl.setText("There was an error uploading your file");
+                    } else if (status == 1) {
+                        statusLbl.setText("file uploaded succesfully");
+                    }
+                    //status popup
+                    GridPane statusPopupGridPane = new GridPane();
+                    statusPopupGridPane.add(statusLbl,0,0);
+                    Scene statusPopupScene = new Scene(statusPopupGridPane);
+                    Stage statusPopupStage = new Stage();
+                    statusPopupStage.setScene(statusPopupScene);
+                    statusPopupStage.show();
+                } catch (Exception e)    {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+
+    }
+
+    private EventHandler<ActionEvent> saveAsAction() {
+        return new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+                GridPane main = new GridPane();
                 Scene saveScene = new Scene(main, 400,300);
                 Stage saveStage = new Stage();
                 saveStage.setScene(saveScene);
+                Label statusLbl = new Label();
                 TextField pathTf = new TextField("Path to file");
                 Button cancelBtn = new Button("Cancel");
                 Button saveBtn = new Button("Save");
                 saveBtn.setOnAction(new EventHandler<ActionEvent>() {
                     @Override
                     public void handle(ActionEvent event) {
+                        header.setText(pathTf.getText());
                         try {
                             Socket socket = new Socket("localhost",8666);
-                            ObjectOutputStream toServer = (ObjectOutputStream) socket.getOutputStream();
-                            ObjectInputStream fromServer = (ObjectInputStream) socket.getInputStream();
+                            ObjectOutputStream toServer = new ObjectOutputStream(socket.getOutputStream());
+                            ObjectInputStream fromServer = new ObjectInputStream(socket.getInputStream());
                             //write 1 to let server know we are saving a file
-                            toServer.write(1);
-                            toServer.writeChars(pathTf.getText());
+                            toServer.writeObject(new Integer(1));
+                            toServer.writeUTF(pathTf.getText());
                             for(Cell[] cellRows : cells)  {
                                 for(Cell cell: cellRows)    {
                                     if(cell.content != "")toServer.writeObject(cell);
@@ -145,10 +261,17 @@ public class ClientDriver extends Application {
 
                             }
                             //send terminator cell
-                            toServer.writeObject(new Cell("-1","",""));
+                            toServer.writeObject(new Cell("","-1",""));
+                            Integer status = (Integer) fromServer.readObject();
+                            if(status == -1)    {
+                                statusLbl.setText("There was an error uploading your file");
+                            }
+                            else if(status == 1)    {
+                                statusLbl.setText("file uploaded succesfully");
+                            }
 
-                        } catch (IOException e) {
-                            main.add(new Label("connection with server unable to be established"),0,10);
+                        } catch (IOException | ClassNotFoundException e) {
+                            statusLbl.setText("error connecting to server");
                             e.printStackTrace();
                         }
                         pathTf.getText();
@@ -157,6 +280,7 @@ public class ClientDriver extends Application {
                 main.add(pathTf, 0,0);
                 main.add(saveBtn, 0,1);
                 main.add(cancelBtn, 1, 1);
+                main.add(statusLbl,0, 10);
 
 
 
@@ -176,6 +300,14 @@ public class ClientDriver extends Application {
         m.find();
         int row = Integer.parseInt(m.group());
         return new int[]{row, StringToNum(column)};
+    }
+    private void clearPage()    {
+        for(int i = 0; i < height; i++) {
+            for(int j = 0; j < width; j++)  {
+                cells[i][j].content = "";
+                cellsTf[i][j].setText("");
+            }
+        }
     }
 
     private int StringToNum(String in)  {
--- main_folder/src/main/java/ServerDriver.java
+++ main_folder/src/main/java/ServerDriver.java
@@ -7,50 +7,127 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class ServerDriver {
-    ServerSocket serverSocket;
+    //public static Connection con;
+    static final String conString = "jdbc:mysql://g84t6zfpijzwx08q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
+    public static Connection con;
+    public static Statement statement;
 
     public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
         ServerSocket serverSocket = new ServerSocket(8666);
         System.out.println("Server started");
-        String conString = "jdbc:mysql://g84t6zfpijzwx08q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
-        Connection con = DriverManager.getConnection(conString, "sill68o7flv9vtch", "pux4n8txd6bdd4j5");
-        System.out.println("connected to DB");
-
+//TODO: impliment open button
         while(true){
+
             Socket socket = serverSocket.accept( );
-            ObjectInputStream fromClient = (ObjectInputStream) socket.getInputStream();
-            ObjectOutputStream toClient = (ObjectOutputStream) socket.getOutputStream();
-            System.out.println("Client connected");
-
-            //operation is an int
-            //0 = load file given path
-            //1 = save file to path
-            int operation = fromClient.readInt();
-            Statement statement = con.createStatement();
-            if (operation == 0)  {
-                //send file given path
-                String path = fromClient.readUTF();
-                ResultSet rs = statement.executeQuery("SELECT id FROM file_id WHERE path_to_file = '"+path+"';");
-                rs.next();
-                int fileId = rs.getInt("id");
-                rs = statement.executeQuery("SELECT row_column, content, style_flags FROM cells WHERE id = '" + fileId + "';");
-                ArrayList<Cell> cells = new ArrayList<>();
-                while (rs.next())   {
-                    cells.add(new Cell(rs.getString("row_column"), rs.getString("content"), rs.getString("style_flags")));
-                }
-            }
-            else if (operation == 1)    {
-                //save file to path
-                ArrayList<Cell> cells = new ArrayList<>();
-                //accept path from client
-                do {
-                    cells.add((Cell) fromClient.readObject());
-                } while(cells.get(cells.size()).row_column != "-1");
-
-
-            }
-
+            new Thread(new HandleClient(socket)).start();
 
         }
+
     }
+    static class HandleClient implements Runnable{
+        Socket socket;
+        public HandleClient(Socket socket)  {
+            this.socket = socket;
+        }
+        @Override
+        public void run() {
+            ObjectOutputStream toClient = null;
+            ObjectInputStream fromClient;
+            try {
+                try {
+                    con = DriverManager.getConnection(conString, "sill68o7flv9vtch", "pux4n8txd6bdd4j5");
+                    statement = con.createStatement();
+                    statement.execute("USE j6hlrczca3i3nsqc");
+                    System.out.println("connected to DB");
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+                fromClient = new ObjectInputStream(socket.getInputStream());
+                toClient = new ObjectOutputStream(socket.getOutputStream());
+                System.out.println("Client connected");
+
+                //operation is an int
+                //0 = load file given path
+                //1 = save file to path
+                Integer operation = (Integer)fromClient.readObject();
+                //open function
+                if (operation == 0) {
+                    String path = (String)fromClient.readObject();
+                    ResultSet rs = statement.executeQuery("SELECT id FROM file_id WHERE path_to_file = '" + path + "';");
+                    if(rs.next()) {
+                        int fileId = rs.getInt("id");
+                        rs = statement.executeQuery("SELECT row_column, content, style_flags FROM cells WHERE id = '" + fileId + "';");
+                        while (rs.next()) {
+                            //cells.add(new Cell(rs.getString("row_column"), rs.getString("content"), rs.getString("style_flags")));
+                            toClient.writeObject(new Cell(rs.getString("row_column"), rs.getString("content"), rs.getString("style_flags")));
+                        }
+                        toClient.writeObject(new Cell("","-1",""));
+                        System.out.println("file " + path + " succesfully sent");
+                    }
+                    else    {
+                        //send error cell if path not found
+                        toClient.writeObject(new Cell("","-2",""));
+                    }
+
+                }
+                //save function
+                else if (operation == 1) {
+                    int fileId;
+                    //accept path from client
+                    String path = fromClient.readUTF();
+                    ResultSet rs = statement.executeQuery("SELECT id FROM file_id WHERE path_to_file = '" + path + "'");
+                    //if rs has next, then the path exists we must delete all cells from database first
+                    if (rs.next()) {
+                        fileId = rs.getInt("id");
+                        PreparedStatement preparedStatement = con.prepareStatement("DELETE FROM cells WHERE id = ?");
+                        preparedStatement.setInt(1,fileId);
+                        preparedStatement.execute();
+
+
+                        //statement.execute("DELETE FROM cells WHERE id = " + fileId);
+                    } else {
+                        statement.executeUpdate("INSERT INTO file_id(path_to_file) VALUES ('" + path + "')");
+                        rs = statement.executeQuery("SELECT id FROM file_id WHERE path_to_file = '" + path + "'");
+                        rs.next();
+                        fileId = rs.getInt("id");
+                    }
+                    do {
+                        Cell currentCell = (Cell) fromClient.readObject();
+                        if (currentCell.content.equals("-1"))
+                            break;
+                        statement.executeUpdate("INSERT INTO cells(row_column, content, style_flags, id) VALUES ('" + currentCell.row_column +
+                                "', '" + currentCell.content + "', '" + currentCell.style_flags + "', '" + fileId + "')");
+                        //cells.add((Cell) fromClient.readObject());
+                    } while (true);
+
+                    System.out.println("file"+ path + " succesfully saved");
+                    toClient.writeObject(new Integer(1));
+                    System.out.println("sucessfully wrote to client status message");
+                }
+
+                //write status back to client
+
+                con.close();
+
+            } catch(Exception e)    {
+                try {
+                    toClient.writeInt(-1);
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void initializeDb()    {
+        try {
+            con = DriverManager.getConnection(conString, "sill68o7flv9vtch", "pux4n8txd6bdd4j5");
+            Statement statement = con.createStatement();
+            System.out.println("connected to DB");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+
 }
